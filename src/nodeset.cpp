fa1ee5ac0abb67dd0a6cfa6412ce0d24d34f38d8
Chris Ying <chrsyng@gmail.com>
2016-04-19 18:03:24 -0400

    more fixes from previous commit

--- src/nodeset.cpp
+++ src/nodeset.cpp
@@ -6,11 +6,11 @@
 bdd::internal::Node* NodeSet::lookupOrCreate(const bdd::internal::Node& node) {
     std::list<bdd::internal::Node*>::iterator it = std::find_if(_set.begin(), _set.end(),
             [&](bdd::internal::Node* elem) {
-                return (node.root == elem->root && node.complemented == elem->complemented && node.branch_true == elem->branch_true && node.branch_false == elem->branch_false);
+                return (node.root == elem->root && node.branch_true == elem->branch_true && node.branch_false == elem->branch_false);
             });
 
     if (it == _set.end()) { // No match found, allocate and insert
-        bdd::internal::Node* new_node = new bdd::internal::Node(node.root, node.complemented, node.branch_true, node.branch_false);
+        bdd::internal::Node* new_node = new bdd::internal::Node(node.root, node.branch_true, node.branch_false);
         _set.push_back(new_node);
         return new_node;
     }
