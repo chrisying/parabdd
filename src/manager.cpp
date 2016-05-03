6c856413199d0ee929cf3d7a31ea5e83ef658317
Matt Bryant <mbryant@andrew.cmu.edu>
2016-05-03 17:22:14 -0400

    Updates

--- src/manager.cpp
+++ src/manager.cpp
@@ -18,13 +18,18 @@ namespace bdd {
 		namespace manager {
             // TODO: we need to set the sizes of these somewhere
 		//	std::unordered_map<Query, Node*> cache;
-			NodeSet nodes(1024);
+			NodeSet nodes;
 
             struct ConstructorHack {
                 ConstructorHack() {
                     // Leave 256 MB for other people, taking at most 16 GB
-                    size_t max_mem = 16 * 1024 * 1024 * 1024;
-                    size_t mem = std::min(mem_available() - (256 * 1024 * 1024), max_mem);
+                    size_t max_mem = 0x400000000;
+                    size_t extra_mem = 0x10000000;
+
+                    size_t cache_size = 0x20000000;
+                    size_t mem = std::min(mem_available() - extra_mem, max_mem) - cache_size;
+
+                    nodes.init(mem);
                     printf("%lu MB\n", mem / 1024 / 1024);
                 }
             } hack;
