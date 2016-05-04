370465ac4b991d16c8f2051dc622daa7d357696a
Chris Ying <chrsyng@gmail.com>
2016-05-04 00:29:39 -0400

    headers TT^TT

--- src/node.h
+++ src/node.h
@@ -47,7 +47,7 @@ namespace bdd {
                 static Node* pointer(Node* node);
 
                 // For debug purposes, will do a full traversal of the tree
-                static void print_node(Node* node);
+                static void print_node(Node* node, int indents);
         };
     }
 }
