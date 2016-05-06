45268c40ac9e1573ce040ae8be14c0c5940d6b19
Chris Ying <cying@andrew.cmu.edu>
2016-05-05 20:22:23 -0400

    int -> long long

--- src/bdd.cpp
+++ src/bdd.cpp
@@ -8,8 +8,8 @@
 #include "bdd.h"
 
 namespace bdd {
-    static long count_sat_helper(internal::Node* node, int n, std::set<Variable>& vars);
     static bool one_sat_helper(internal::Node* node, bool p, std::unordered_map<Variable, bool>& map);
+    static long long count_sat_helper(internal::Node* node, int n, std::set<Variable>& vars);
 
     Bdd Bdd::bdd_true(internal::Node::true_node);
     Bdd Bdd::bdd_false(internal::Node::false_node);
@@ -112,10 +112,10 @@ namespace bdd {
      * Count SAT
      **/
 
-    int Bdd::count_sat(std::set<Variable> vars) {
+    long long Bdd::count_sat(std::set<Variable> vars) {
         int n = vars.size();
-        long pow2 = pow(2, n);
-        long count = count_sat_helper(this->node, n, vars);
+        long long pow2 = pow(2, n);
+        long long count = count_sat_helper(this->node, n, vars);
 
         if (count == -1) {
             std::cout << "A variable in the BDD was not declared in vars" << std::endl;
@@ -133,9 +133,9 @@ namespace bdd {
     // should be that count_sat_helper will return the exact number of SAT
     // assignments. We should do the negations at the base case and right
     // before adding to cache.
-    static long count_sat_helper(internal::Node* node, int n, std::set<Variable>& vars) {
+    static long long count_sat_helper(internal::Node* node, int n, std::set<Variable>& vars) {
         // TODO: handle overflow by using real doubles
-        long pow2 = pow(2, n);
+        long long pow2 = pow(2, n);
         if (internal::Node::is_leaf(node)) {
             return pow2;
         }
@@ -148,8 +148,8 @@ namespace bdd {
         }
 
         // TODO: this can be done in parallel
-        int countT = count_sat_helper(dnode->branch_true, n, vars);
-        int countF = count_sat_helper(dnode->branch_false, n, vars);
+        long long countT = count_sat_helper(dnode->branch_true, n, vars);
+        long long countF = count_sat_helper(dnode->branch_false, n, vars);
 
         if (countT == -1 || countF == -1) {
             return -1; // TODO: should -1 be cached?
@@ -159,7 +159,7 @@ namespace bdd {
             countT = pow2 - countT;
         }
 
-        long count = (countT + countF) / 2;
+        long long count = (countT + countF) / 2;
 
         // TODO: add to cache now
 
